8d1113e3c027ca7ec98fe51ca488ae4b6229874f
Huzaifa Zafar <creativehuzaifa97@gmail.com>
2024-03-25 05:02:19 +0500

    Store updated

--- src/Pages/HeaderNav.jsx
+++ src/Pages/HeaderNav.jsx
@@ -54,7 +54,6 @@ function HeaderNav() {
                     </span>
                 </div>
             </nav>
-            <ToastContainer />
         </div>
     )
 }
--- src/Pages/ProductsDetail.jsx
+++ src/Pages/ProductsDetail.jsx
@@ -48,7 +48,6 @@ const ProductsDetail = () => {
                     )
                 }
             </center>
-            <ToastContainer />
         </div>
     )
 }
